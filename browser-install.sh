78f480f4b15f8569539f62fb44f8fae05168b339
Serg <sergstep314@gmail.com>
2020-11-04 21:21:13 +0400

    Create browser-install.sh

---
+++ browser-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+xdg-mime default google-chrome.desktop x-scheme-handler/http
+xdg-mime default google-chrome.desktop x-scheme-handler/https
+xdg-mime default google-chrome.desktop text/html
