61fd2795bf6756679267b8e3a1a5acd7b161dcb4
Sergey Stepanyan <sergstep314@gmail.com>
2021-06-23 17:48:32 +0400

    Add UPS script.

---
+++ ups/detect-powerloss.sh
@@ -0,0 +1,19 @@
+
+i=0
+while [ True ] ;do
+    result=$(xrandr --verbose | grep -i 960x540)
+    if [ ! -z "$result" ];then
+        #Secondary monitor off - power loss
+        echo "$i"
+        i=$((i+1))
+        if [ $i -ge 90 ];then
+            #Suspend after 15 minutes
+            i=0
+            systemctl suspend;
+        fi
+    else
+        i=0
+    fi
+
+    sleep 10
+done
