cddca3aa7424d557b05a0b3b459c8800c66c3451
Serg <sergstep314@gmail.com>
2022-11-19 01:00:21 +0400

    Fix bgpcheck

--- bgpcheck.sh
+++ bgpcheck.sh
@@ -3,8 +3,8 @@
 prefix=$1
 
 res=$(curl -s "https://lg.de-cix.net/api/v1/lookup/prefix?q=$prefix")
-res=$(echo $res | jq '.imported.routes[0].neighbor.asn')
-if [ -z "$res" ];then
+res=$(echo -n $res | jq '.imported.routes[0].neighbor.asn')
+if [ ! -z "$res" ];then
     echo $res
     exit 0
 else
