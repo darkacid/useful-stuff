8b0a6df627e0e8f51ec17639f02280867a471d2e
Serg <sergstep314@gmail.com>
2023-02-10 18:05:08 +0400

    Changing setfanspeed.sh

--- nvidia/setfanspeed.sh
+++ nvidia/setfanspeed.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-
+export DISPLAY=:0
+speed=40
+if [[ ! -z $1 ]];then
+    speed=$1
+fi 
 
 nvidia-settings -a [gpu:0]/GPUFanControlState=1
-nvidia-settings -a [fan:0]/GPUTargetFanSpeed=$1
+nvidia-settings -a [fan:0]/GPUTargetFanSpeed=$speed
