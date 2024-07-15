7d7cea120e83e5af98cc93000e885c12b4571170
Serg <sergstep314@gmail.com>
2024-07-16 01:16:18 +0400

    Update UPS service logic.

--- ups/http_server.py
+++ ups/http_server.py
@@ -1,34 +1,56 @@
+#!/usr/bin/env python3
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
 import threading
 import time
 
 
-status = True # Host is up by default 
+status = True # Host is up by default
+
 
 def ping(host:str,count=3,timeout=6):
     #Return true if always up, false otherwise
     res = os.system("ping -t {} -c {} {} >/dev/null".format(timeout,count,host))
     return res==0
 
+def bat_status():
+    with open("/sys/class/power_supply/BAT1/status",mode='r') as status:
+        bat = status.read().strip()
+    return bat != "Discharging"
+
 def update_status():
     global status
+    
     while True:
-        new_status = ping("192.168.1.1")
+        new_status = bat_status()
+        
         if status != new_status:
-            if new_status == True:
+            if new_status:
                 print("Power is ON now")
+                status = new_status
             else:
-                print("Power went out. Sleeping for 30 seconds before changing status")
-                time.sleep(30)
-            status = new_status
-
-        time.sleep(15)
+                print("Power went out. Checking for 10 seconds before changing status")
+                power_restored = False
+                
+                for _ in range(10):
+                    time.sleep(1)
+                    if bat_status():
+                        power_restored = True
+                        print("Power restored during the 10 seconds check")
+                        break
+                
+                if not power_restored:
+                    status = False
+                    print("Power didn't come back. Status set to False")
+        
+        time.sleep(5)
 
 def check_status():
     return status
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def log_message(self, format, *args):
+        pass
     def do_GET(self):
         if check_status():
             self.send_response(200)
